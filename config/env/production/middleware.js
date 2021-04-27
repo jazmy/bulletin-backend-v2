2e70b081931b669d970b103001e2d167f4560238
Jasmine Robinson <jaz@jazmy.com>
2021-04-26 19:21:09 -0700

    updated cors

--- config/env/production/middleware.js
+++ config/env/production/middleware.js
@@ -6,6 +6,7 @@ module.exports = {
 				"http://localhost:3000",
 				"http://localhost:1337",
 				"https://bulletin-backend-strapi-new.herokuapp.com",
+				"https://bulletin-backend-strapi-v2.herokuapp.com",
         		"https://bulletinfrontend.netlify.app",
 				"https://mystifying-elion-d2dea9.netlify.app",
 				"https://zen-hamilton-431082.netlify.app"
